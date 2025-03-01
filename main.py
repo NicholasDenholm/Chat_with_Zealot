dd8dab852c3a2cc4d371858dd19f23dd3a71cc7f
NicholasDenholm <nicholas.denholm1@gmail.com>
2025-03-01 12:22:22 -0500

    modified:   .gitignore 	modified:   main.py

--- main.py
+++ main.py
@@ -20,36 +20,41 @@ def main():
 
 
 
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    dataloader = imp.torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
     vocab_size = len(dataset.chars)
     
-    model = TextModel(vocab_size, hidden_size, sequence_length)
+    model = imp.TextModel(vocab_size, hidden_size, sequence_length)
     
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = imp.nn.CrossEntropyLoss()
+    optimizer = imp.optim.Adam(model.parameters(), lr=learning_rate)
     
-    train(model, dataloader, criterion, optimizer, num_epochs)
+    trainee = imp.Train()
+    trainee.train(model, dataloader, criterion, optimizer, num_epochs)
 
     
     # Get the current date to append to the filename so overwrites dont happen
     # Format as YYYY-MM-DD_Hour-Min-Second
-    current_date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  
+    current_date = imp.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  
     output_path = load_inst.set_output_folder()
-    model_save_path = os.path.join(output_path, f'text_model_{current_date}.pth')
+    model_save_path = imp.os.path.join(output_path, f'text_model_{current_date}.pth')
     # After training, save the model
-    torch.save(model.state_dict(), model_save_path)
+    imp.torch.save(model.state_dict(), model_save_path)
     
 
     # Generate text after training
+    generator = imp.Generate_Text()
     seed_text = "Once upon a time"
-    generated_text = generate_text(model, seed_text, sequence_length, 500, dataset.char_to_idx, dataset.idx_to_char)
+    generated_text = generator.generate_text(model, seed_text, sequence_length, 500, dataset.char_to_idx, dataset.idx_to_char)
     print(generated_text)
 
     # Load the model and generate text
-    loaded_model = TextModel(vocab_size, hidden_size=256, sequence_length=sequence_length)
-    loaded_model.load_state_dict(torch.load('text_model.pth'))
+    loaded_model = imp.TextModel(vocab_size, hidden_size=256, sequence_length=sequence_length)
+    loaded_model.load_state_dict(imp.torch.load('text_model.pth'))
     loaded_model.eval()
     
+    # Generate text from the loaded model
+    generated_text = generator.generate_text(loaded_model, seed_text, sequence_length, 500, dataset.char_to_idx, dataset.idx_to_char)
+    print(generated_text)
 
 
 if __name__ == '__main__':
