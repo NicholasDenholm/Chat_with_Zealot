6bdac0bbe174417645c3fe483694d8e9431587ff
NicholasDenholm <nicholas.denholm1@gmail.com>
2025-05-18 17:35:07 -0400

    modified:   .gitignore 	modified:   Cudatest.py 	modified:   requirements.txt

--- Cudatest.py
+++ Cudatest.py
@@ -1,18 +1,15 @@
-# Do this: pip install torch==1.12.0+cu118 torchvision==0.13.0+cu117 torchaudio==0.12.0 -f https://download.pytorch.org/whl/torch_stable.html
-
-# ran this instead: pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1
-
-# Did this: pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-# Try this: python -c "import torch; print(torch.rand(2,3).cuda())"
-
 import torch
-
 print(torch.__version__)
 print(torch.cuda.is_available())  # Should return True if CUDA is available
 #print(torch.cuda.get_device_name(0))  # Should show the name of the GPU
 
-# Switching between versions:
+# Attempts
+# Do this: pip install torch==1.12.0+cu118 torchvision==0.13.0+cu117 torchaudio==0.12.0 -f https://download.pytorch.org/whl/torch_stable.html
+# ran this instead: pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1
+# Did this: pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+# Try this: python -c "import torch; print(torch.rand(2,3).cuda())"
 
+# Switching between versions:
 '''
 11.7
 export PATH=/usr/local/cuda-11.7/bin:$PATH
