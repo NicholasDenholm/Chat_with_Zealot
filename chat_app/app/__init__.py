ffaaa053ed814ef7481fde6f87bd5d28b2697c79
NicholasDenholm <nicholas.denholm1@gmail.com>
2025-06-29 12:19:53 -0400

    Seperated front end

--- chat_app/app/__init__.py
+++ chat_app/app/__init__.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from .bot import load_chat_model, init_chat_state
+from .bot import init_model_state
 from .routes import register_routes
 
-def create_app(model:str):
+def create_app(backend:str, model:str):
     # retrive html/css/js scripts from static and templates folder
     templates_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'templates'))
     static_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'static'))
@@ -12,7 +12,7 @@ def create_app(model:str):
     app = Flask(__name__, template_folder=templates_path, static_folder=static_path)
     CORS(app)
 
-    app.config['state'] = init_chat_state(model) 
+    app.config['state'] = init_model_state(backend, model) 
 
     # Register all routes
     register_routes(app)
--- chat_app/app/routes.py
+++ chat_app/app/routes.py
@@ -1,10 +1,47 @@
 from flask import request, jsonify, render_template, current_app
-from .bot import chat_with_speech
+from prebuilt.app.chat_bot import chat_with_speech
+from prebuilt.app.audio_bot import Whisper_Bot
+import tempfile
+import os
+
+#from bots import whisper_bot
 
 ### a route is how the app knows what code to run when a user accesses a certain URL.
 
 def register_routes(app): # regester_routes called with __init__, keeps chat and home modular
     
+    whisper_bot = Whisper_Bot(model_name="base")
+
+    @app.route('/api/audio', methods=["POST"])
+    def api_audio():
+        if 'audio' not in request.files:
+            return jsonify({"error": "No audio file provided"}), 400
+
+        audio_file = request.files['audio']
+
+        # Save to temp file
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp:
+            audio_path = temp.name
+            audio_file.save(audio_path)
+
+        try:
+            transcription = whisper_bot.transcribe_audio(audio_path)
+            chat_state = current_app.config['state']
+            
+            bot_response = chat_with_speech(transcription, chat_state)
+
+            return jsonify({
+                "transcription": transcription,
+                "response": bot_response
+            })
+
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+
+        finally:
+            os.remove(audio_path)
+
+
     @app.route("/chat", methods=["POST"])
     def chat():
         data = request.json
